e9d27c191314b21f6316045ab58e87f1d20a45fd
Kunal Mundra <kunalmundra2@gmail.com>
2023-09-09 15:20:20 +0530

    Featured post design added

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from "./featured.module.css"
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Kunal here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.PostTitle}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+          <p className={styles.PostDesc}>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
