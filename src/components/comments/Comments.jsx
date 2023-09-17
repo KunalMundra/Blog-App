d2be49ec499e25f0902a842a54cc232b47abf250
Kunal Mundra <kunalmundra2@gmail.com>
2023-09-18 02:06:39 +0530

    Create a comment functionality added

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import React from 'react'
-import styles from "./comments.module.css"
-import Link from 'next/link'
-import Image from 'next/image'
-import useSWR from 'swr'
-import { useSession } from 'next-auth/react'
+import Link from "next/link";
+import styles from "./comments.module.css";
+import Image from "next/image";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 const fetcher = async (url) => {
     const res = await fetch(url);
@@ -21,17 +21,36 @@ const fetcher = async (url) => {
 };
 
 const Comments = ({ postSlug }) => {
-    const {status} = useSession();
+    const { status } = useSession();
 
     const { data, mutate, isLoading } = useSWR(
-        `http://localhost:3000/api/comments?postSlug=${postSlug}`, fetcher);
+        `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+        fetcher
+    );
+
+    const [desc, setDesc] = useState("");
+
+    const handleSubmit = async () => {
+        await fetch("/api/comments", {
+            method: "POST",
+            body: JSON.stringify({ desc, postSlug }),
+        });
+        mutate();
+    };
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Comments</h1>
             {status === "authenticated" ? (
                 <div className={styles.write}>
-                    <textarea placeholder="Write a comment" className={styles.input}></textarea>
-                    <button className={styles.button}>Send</button>
+                    <textarea
+                        placeholder="Write a comment..."
+                        className={styles.input}
+                        onChange={(e) => setDesc(e.target.value)}
+                    />
+                    <button className={styles.button} onClick={handleSubmit}>
+                        Send
+                    </button>
                 </div>
             ) : (
                 <Link href="/login">Login to write a comment</Link>
